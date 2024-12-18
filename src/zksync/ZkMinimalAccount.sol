50c85821e46bf2c658383854ee2655f0a2ff7bc8
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-12-18 15:50:22 +0100

    validate transaction

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {IAccount} from "cyfrin-foundry-era-contracts/interfaces/IAccount.sol";
-import {Transaction} from "cyfrin-foundry-era-contracts/libraries/MemoryTransactionHelper.sol";
+import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "cyfrin-foundry-era-contracts/interfaces/IAccount.sol";
+import {INonceHolder} from "cyfrin-foundry-era-contracts/interfaces/INonceHolder.sol";
+import {
+    Transaction, MemoryTransactionHelper
+} from "cyfrin-foundry-era-contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "cyfrin-foundry-era-contracts/libraries/SystemContractsCaller.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS} from "cyfrin-foundry-era-contracts/Constants.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * Lifecycle of a type 113 (0x71) transaction
@@ -22,16 +29,40 @@ import {Transaction} from "cyfrin-foundry-era-contracts/libraries/MemoryTransact
  * 8. The main node calls executeTransaction
  * 9. If a paymaster was used, the postTransaction is called
  */
-contract ZkMinimalAccount is IAccount {
+contract ZkMinimalAccount is IAccount, Ownable {
+    using MemoryTransactionHelper for Transaction;
+
+    error ZkMinimalAccount__NotEnoughBalance();
+    error ZkMinimalAccount__NotFromBootLoader();
+
+    modifier requireFromBootLoader() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
+            revert ZkMinimalAccount__NotFromBootLoader();
+        }
+        _;
+    }
+
+    constructor() Ownable(msg.sender) {}
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @notice must increase the nonce
+     * @notice must validate the transaction (check the owner signed the transaction)
+     * @notice also check to see if we have enough money in our account
+     */
     function validateTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
+        bytes32, /*_txHash*/
+        bytes32, /*_suggestedSignedHash*/
         Transaction memory _transaction
     )
         external
         payable
         returns (bytes4 magic)
-    {}
+    {
+        return _validateTransaction(_transaction);
+    }
 
     function executeTransaction(
         bytes32 _txHash,
@@ -61,4 +92,44 @@ contract ZkMinimalAccount is IAccount {
         external
         payable
     {}
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function _validateTransaction(Transaction memory _transaction) internal returns (bytes4 magic) {
+        _incrementNonce(_transaction);
+        _checkForFeeToPay(_transaction);
+        magic = _checkSignature(_transaction);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function _incrementNonce(Transaction memory _transaction) private {
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce))
+        );
+    }
+
+    function _checkForFeeToPay(Transaction memory _transaction) private view {
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if (totalRequiredBalance > address(this).balance) {
+            revert ZkMinimalAccount__NotEnoughBalance();
+        }
+    }
+
+    function _checkSignature(Transaction memory _transaction) private view returns (bytes4 magic) {
+        bytes32 txHash = _transaction.encodeHash();
+        address signer = ECDSA.recover(txHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+        return magic;
+    }
 }
